d4280ad8a067fc7dee0fb354b83e33329e073244
PRSDWUM <129490524+PRSDWUM@users.noreply.github.com>
2025-03-13 16:51:03 +0800

    Update GAT_layers.py

--- GAT_layers.py
+++ GAT_layers.py
@@ -22,51 +22,19 @@ class GraphAttentionLayer(nn.Module):
         nn.init.xavier_uniform_(self.a.data, gain=1.414)
 
         self.leakyrelu = nn.LeakyReLU(self.alpha)
-# 
-#     def forward(self, h, adj, split_name):
-#         Wh = torch.mm(h, self.W) # h.shape: (N, in_features), Wh.shape: (N, out_features)
-#         e = self._prepare_attentional_mechanism_input(Wh)
-#         
-#         zero_vec = -9e15*torch.ones_like(e)
-#         zero = torch.ones_like(e)-1
-#         if split_name.startswith("train"):
-#             thre = 0
-#             attention = torch.where(adj > thre, e, zero_vec)
-#             
 
-#         else:
-#             thre = 0.4
-#             attention = torch.where(e > thre, e, zero_vec)
-#             adj = torch.where(e > thre, torch.ones_like(e), zero)
-            
-
-#         attention = F.softmax(attention, dim=1)
-#         attention1 = F.dropout(attention, self.dropout, training=self.training)
-#         attention = torch.where(adj>thre,attention1,zero)
-#         h_prime = torch.matmul(attention, Wh)
-
-#         if self.concat:
-#             return F.elu(h_prime)
-#         else:
-#             return h_prime
     def forward(self, h, adj, split_name):
         Wh = torch.mm(h, self.W) # h.shape: (N, in_features), Wh.shape: (N, out_features)
         e = self._prepare_attentional_mechanism_input(Wh)
         
         zero_vec = -9e15*torch.ones_like(e)
         zero = torch.ones_like(e)-1
-#         if split_name.startswith("train"):
-#             attention = torch.where(adj > 0, e, zero_vec)
-#             
 
-#         else:
-#             attention = e
-#             #adj = torch.ones_like(e)
         attention = torch.where(adj > 0, e, zero_vec)
     
         attention1 = F.softmax(attention, dim=1)
         attention2 = F.dropout(attention1, self.dropout, training=self.training)
-        attention3 = torch.where(adj > 0, attention2, zero)# 只有这一句是变动的了
+        attention3 = torch.where(adj > 0, attention2, zero)
         h_prime = torch.matmul(attention3, Wh)
 
         if self.concat:
